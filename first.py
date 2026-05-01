b115eaa2135c74e565e126f5382cfa8cd2a5e086
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-01 16:19:39 +0530

    abhinay

---
+++ first.py
@@ -0,0 +1,13 @@
+n1=20
+n2=30
+n1, n2 = n2, n1
+#print(f"n1={n1} n2={n2}")
+char1 = "hello"
+char2 = "java"
+char1, char2 = char2, char1
+#print(f"char1={char1} char2={char2}")
+n1 = 200
+char2 = "java"
+n1, char2 = char2, n1
+#print(f"n1={n1} char2={char2}")
+
