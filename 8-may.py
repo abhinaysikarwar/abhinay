7297419f7af5f75e6e62d035773f524f9eed16df
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-08 11:29:09 +0530

    8may

---
+++ 8-may.py
@@ -0,0 +1,81 @@
+# Loops in python 
+#1. for loop : ranged based
+#2. while loop : condition based
+
+
+# 1. for loop 
+# range always works with integer number.
+# range (start, stop -1, step-1):
+
+# for i in range(1,15,2):
+#     print(i)
+
+# for i in range(4,10,1):
+#     print (i,end=" ")
+
+# for i in range(4,10,1):
+#     print (i,end=" ")
+#     print("python")
+
+# for i in range(1,20):
+#     if  i==10:
+#         continous   
+#         print(i)
+
+# for i in range(1,21):
+#     if i%2==0:
+#         print(i,end=" ")
+
+
+# s=0
+# for i in range(1,5):
+#     s=s+1
+#     print(s)
+
+# for r in range(10,0,-1):
+#     print(r)
+
+
+# for i in range(1,15):
+#     if i==5:
+#         break
+#     print(i)
+
+# for i in range(1,15):
+#     if i==5:
+#         continue
+#     print(i)
+
+
+# 1.swap to takes start_point and end_point from user input
+# and print all number divisble by 2 and 3.
+
+# 2. wap to take a number from use input and print formated table.
+# format :
+# 3x1=3
+# 3x2=6
+
+#.....
+#3x10=30
+
+# 3. wap to take a number from user input and print reversed formated table.
+
+
+# start_point=int(input("Enter start point :"))
+# end_point=int(input("Enter end point :"))
+# print("number divisble by 2 and 3")
+# for num in range (start_point,end_point, +1):
+#     if num % 2 ==0 and num % 3 ==0:
+#         print(num)
+
+# num=int(input("Enter number :"))
+# for i in range(1,11):
+#     print(f"{num} * {i} = {num*i}")
+
+# num=int(input("Enter the number :"))
+# for i in range(1,11):
+#     print(f"{num} * {i} = {num*i}")
+
+# num=int(input("Enter the number :"))
+# for i in range(10,0,-1):
+#     print(f"{num} * {i} = {num * i}")
