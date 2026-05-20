f7795f6000f435da9896be1af8d79a5866bdd7ac
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-20 10:38:28 +0530

    function

--- 19-may.py
+++ 19-may.py
@@ -93,3 +93,90 @@
 
 # def user_name(a):
 #     retrun a
+
+
+# waf to check number pass by by argument is odd or even :
+
+# def check_number(x):
+#     if x % 2  ==0:
+#         print("Even number")
+#     else:
+#         print("Odd number")
+# check_number(5)
+
+# # waf to check which is greater and two numver by user .
+
+# def add(a,b):
+#     if a>b :
+#         print("Greater A")
+#     else:
+#         print("Greater B")
+# add(35,65)
+# add(45,35)
+
+
+# a=int(input("enter the number :"))
+# b=int(input("enter the number :"))
+# def add(a,b):
+#     if a>b :
+#         print("Greater A")
+#     else:
+#         print("Greater B")
+# add(a,b)
+
+
+# waf to check the charecter pass by user is vowel or consonant.
+
+# def check_character(x):
+#     if x in ("aeiou"):
+#         print ("vowel")
+#     else:
+#         print("constant")
+# b="a"
+# check_character(b)
+
+# waf to check is number completly divide by 2 and 3 and return :
+
+# "yes number is completely divided"
+# "No number is not completely divide"
+
+# def check_number(a):
+#     if a % 2==0 and a % 3==0:
+#         print("yes number is completely divide")
+#     else:
+#         print("No number is not completely divide")
+# b=30
+# check_number(b)
+
+
+# def check_number(a):
+#     if a % 2==0 and a % 3==0:
+#         return "yes number is completely divide"
+#     else:
+#         return "No number is not completely divide"
+# b=30
+# res=check_number(b)
+# print(res)
+
+
+# waf to return length of a string pass by user without using len().
+
+# def len_string(s):
+#     c=0
+#     for i in s:
+#         c=c+1
+#     return c
+# res=len_string("python")
+# print(res)
+
+
+
+def len_string(s):
+    print(s)
+    c=0
+    for i in s:
+        print(i)
+        c=c+1
+    return c
+res=len_string("python")
+print(res)
