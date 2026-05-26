32244c4945beb03268395cd2ab35532530a455af
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-26 21:35:47 +0530

    list

---
+++ 26-may.py
@@ -0,0 +1,45 @@
+# Data structure.
+# Data structres used to store data effeciently and make faster for operations like read and write.
+
+# 1. List : list ()
+# 2. string : str()
+# 3. Dictionary : dict()
+# 4. set : set()
+# 5. tuple : tuple()
+
+######### 1. List 
+# 1. List is a data structure in python used to store multiple data of different types in one variable .
+
+# 2. List ca  define by useing square [] and data inside known as element .
+
+# 3. List can be hetrogenous and homogenuous .
+
+# 4. List are mutable (changeable)
+
+# 5. List support indexing , slicing and follows ordering sequence. 
+
+# marks_10th=[20,55,60,45,30]
+# print("Before Update : ", marks_10th)
+# marks_10th[0]=200   # mutating list element using index .
+# print("After update : ",marks_10th)
+
+# marks_10th=[20,55,60,45,30]
+# print("Before Update : ", marks_10th)
+# marks_10th[-1]=200
+# print("After update : ",marks_10th)
+
+# marks_10th=[20,55,60,45,30]
+# print("Before Update : ", marks_10th)
+# marks_10th[-2]=200
+# print("After update : ",marks_10th)
+
+# marks_10th=[20,55,60,45,30]
+# print("Before Update : ", marks_10th)
+# marks_10th[2]=200
+# print("After update : ",marks_10th)
+
+
+# marks_10th=[10,20,30,40]
+# i=2-1
+# print(marks_10th[3])
+
---
+++ structurepractice.py
@@ -0,0 +1,108 @@
+#  for list practice :
+
+# nums =[1,2,3,4,5]
+# print(nums)
+
+# nums=[1,2,3,4,5]
+# i=3
+# print(nums[i])
+
+# fruits=["Apple","Orange","Banana","Mango"]
+# print(fruits)
+
+
+# fruits=["Apple","Orange","Banana","Mango"]
+# print("Before update :", fruits)
+# fruits[-2]
+# print("After update :", fruits[-2])
+
+# fruits=["Apple","Orange","Banana","Mango"]
+# fruits.remove("Orange")     # remove this in list hata deta hai
+# print(fruits)
+
+
+# fruits=["Apple","Orange","Banana","Mango"]
+# fruits.append("Orange")       # append add one new in list
+# print(fruits)
+
+
+# items=[1,2,3,4,5]
+# print(len(items))
+
+
+# list ke saare elements for loop se print karo.
+
+# colors=["Red", "blue", "Green"]
+# for i in colors :
+#     print(i)
+
+# list ka total sum nikalo.
+
+# nums=[2,5,8,5,25]
+# total=0
+# for i in nums:     # "+="  this is total of sum 
+#     total+=i
+# print("Total=",total)
+
+# list me sabse bada number print karo :
+
+# nums=[34,23,43,12,23]
+# largest=nums[0]
+# for i in nums:
+#     if i > largest:
+#         largest = i
+# print(largest)
+    
+
+# nums=[2,6,9,15,60]
+# largest=nums[0]
+# for i in nums:
+#     if i > largest:
+#         largest = i
+# print(largest)
+
+# nums=[56,78,100,54,67]
+# largest=nums[0]
+# for i in nums:
+#     if  i > largest :
+#         largest = i
+# print(largest)
+
+# List me sirf even numbers print karo :
+
+# nums=[23,56,34,78,10]
+# smallest=nums[0]
+# for i in nums:
+#     if i < smallest:
+#         smallest = i
+# print(smallest)
+
+# nums=[56,78,23,43,34]
+# smallest=nums[0]
+# for i in nums:
+#     if i < smallest :
+#         smallest = i
+# print(smallest)
+
+# list me sirf enen numbers print karo :
+
+# nums=[1,2,3,4,5,6,7,8,9]
+# for i in nums:
+#     if i % 2 == 0:
+#         print(i,end=" ")
+
+# nums=[1,2,3,4,5,6,7,8,9]
+# for i in nums:
+#     if i % 2 != 0:
+#         print(i,end=" ")
+
+
+# diffrent type of list questions :
+# Nested list :
+
+# data=[[1,2],[3,4],[5,6]]
+# for i in data :
+#     for j in i :    # this is wrong code 
+#         print(j)
+
+
