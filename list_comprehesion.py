3da8ae9bb406b77389bf2cb01195a47ba125b0b5
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-06-12 10:33:48 +0530

    list_comprehesion

---
+++ list_comprehesion.py
@@ -0,0 +1,39 @@
+# emp_list
+# for i in range(1,11):
+#     emp_list.append(i)
+# pritn(emp.list)
+
+# print([i**2 for i in range(1,11)])
+# print([i for i in range(1.11) if i%2==0])
+# print([str(i)+":Even" if i%2==0 else str(i)+":ODD" for i in range(1,11)])
+
+# emp_name=["aman","shivan","SHIVAM"]
+# res=[n.lower() for n in emp_name]
+# res=[n.upper() for n in emp_name]
+# res=["-".join(n) for n in emp_name]
+# print(res)
+
+
+# fruits_list=["apple","mango","papaya","banana","orange","grapes"]
+# for i in fruits_list:
+#     if n in fruits_list :
+# print(fruits_list)
+
+# fruits_list=["apple","mango","papaya","banana","orange","grapes"]
+# user="p"
+# res=d[i.upper()  i in fruits_list if serch in p]
+# print(res)
+
+
+#write to check 0dd_even number
+
+# def odd_even(n):
+#     if n % 2 ==0:
+#         return "Even number"
+#     else:
+#         return "Odd number"
+# res=odd_even(12)
+# print(res)
+
+res=lambda x,y : x+y
+print(res(10,20))
