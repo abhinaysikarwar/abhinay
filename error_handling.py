35f29572e149cb741ef368fd7b276cd895d4f82d
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-06-12 09:38:17 +0530

    error_handling

---
+++ error_handling.py
@@ -0,0 +1,22 @@
+# try:
+#     n1=10
+#     n2=2
+#     res=n1/n2
+#     print(res)
+# except Exception as e:
+#     print("Error : ",e)
+
+# finally: 
+#     print("Thanks for visiting ......")
+
+
+# try:
+#     n1=10
+#     n2=0
+#     res=n1/n2
+#     print(res)
+# except Exception as e:
+#     print("Error : ",e)
+
+# finally: 
+#     print("Thanks for visiting ......")
